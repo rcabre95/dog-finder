e550c61ccb4352925860e4ded9e0395f4c8b463f
rcabre95 <raphael.cabrera111@gmail.com>
2023-05-13 23:02:42 -0500

    added Match and Steps components

---
+++ components/Match.tsx
@@ -0,0 +1,8 @@
+export default function Match() {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ components/Steps.tsx
@@ -0,0 +1,13 @@
+export default function Steps() {
+
+    return (
+        <div className={``}>
+            <h3>Get matched with your furry friend in 3 easy steps!</h3>
+            <ol>
+                <li>Step 1</li>
+                <li>Step 2</li>
+                <li> Step 3</li>
+            </ol>
+        </div>
+    )
+}
