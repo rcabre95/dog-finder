13f73cfb003d3731b5c32933d494229e07ea327f
rcabre95 <raphael.cabrera111@gmail.com>
2023-05-15 17:03:44 -0500

    created initial data load in useEffect hook

--- pages/dogs.tsx
+++ pages/dogs.tsx
@@ -3,12 +3,14 @@ import { Dog } from "@/lib/dogs";
 import { GetServerSideProps } from "next"
 import { useState, useEffect } from "react";
 import { SDK } from "@/lib/fetch_sdk";
+import { Geo, MapPoint } from "@/lib/utils/distance";
+import LogoutBtn from "@/components/shared-ui/LogoutBtn";
 
 // export const getServerSideProps: GetServerSideProps = async () => {
-//     await SDK.login("Bob", "Bob@bob.com");
-//     const breeds: Array<string> = await SDK.getBreeds();
+//     // await SDK.login("Bob", "Bob@bob.com");
+//     await SDK.fetchBreeds();
 //     // const dogs: Array<string> = await SDK.doDogSearch();
-//     // const breeds: Array<string> = [];
+//     const breeds: Array<string> = [];
 //     const dogs: Array<string> = [];
 //     console.log(SDK.temp);
 
@@ -20,12 +22,16 @@ import { SDK } from "@/lib/fetch_sdk";
 //     }
 // }
 
+
 // export default function Dogs({ dogs, initBreeds }: { dogs: Array<Dog>, initBreeds: Array<string> }) {
 export default function Dogs() {
 
-    const [breeds, setBreeds] = useState<Array<string>>();
+    const [breeds, setBreeds] = useState<Array<string>>([]);
     const [page, setPage] = useState<number>(1);
+    const [locationAvailable, setLocationAvailable] = useState<boolean>(true);
+    const [location, setLocation] = useState<Array<number>>()
     const [showFilters, setShowFilters] = useState<boolean>(false);
+    const [dogs, setDogs] = useState<Array<string>>([])
 
     const prevPage = async () => {
         
@@ -34,18 +40,63 @@ export default function Dogs() {
     const nextPage = async () => {
 
     }
-
     
     useEffect(() => {
+        // var initZipCodes: string[] = ["60630"]
         SDK.getBreeds().then((d) => {
-            setBreeds(d)
-        })
-        
+            setBreeds(d);
+        });
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async (position) => {
+                const coords: MapPoint = {
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                }
+                const { minPoint, maxPoint } = Geo.getBoundingBox(coords, 20);
+                // console.log([minPoint, maxPoint])
+                SDK.getZipcodes(minPoint, maxPoint).then((zipCodes) => {
+                    SDK.getDogs(breeds, zipCodes).then((d) => {
+                        // console.log(d)
+                        setDogs(d)
+                    })
+                    
+                })
+            })
+        } 
+        console.log(dogs)
+        // navigator.geolocation.getCurrentPosition(
+        //     async function(position){
+        //         // console.log(position.coords)
+        //         const coords: MapPoint = {
+        //             lat: position.coords.latitude,
+        //             lon: position.coords.longitude
+        //         }
+                
+        //         // console.log(coordinates)
+        //         console.log(Geo.getBoundingBox(coords, 10))
+        //         const { minPoint, maxPoint } = Geo.getBoundingBox(coords, 20);
+        //         SDK.getZipcodes(minPoint, maxPoint).then((d) => {
+        //             initZipCodes
+        //         })
+        //     },
+        //     function(error) {
+        //         if (error.code !== 0) {
+        //             setLocationAvailable(false)
+        //         }
+        //     }
+        // );
+
+        //         SDK.getDogs(breeds, initZipCodes).then((d) => {
+        //             setDogs(d)
+        //         })
+
     }, [])
 
     return (
         <div className={``}>
+            <LogoutBtn />
             {breeds ? <div>{JSON.stringify(breeds)}</div> : null}
+            {dogs ? <div>{JSON.stringify(dogs)}</div> : null}
         </div>
     )
 }
