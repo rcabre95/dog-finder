3984db8c5689ec3a4a603668d1a77f2674cbc18a
rcabre95 <raphael.cabrera111@gmail.com>
2023-05-16 14:25:24 -0500

    added content to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,39 @@
+import Image from "next/image"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+
 export default function NotFound() {
 
+    const router = useRouter();
+    const [timeLeft, setTimeLeft] = useState<number>(10);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (timeLeft > 0) {
+                setTimeLeft(timeLeft - 1);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [timeLeft])
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            router.push('/')
+        }
+    }, [timeLeft])
+
     return (
-        <div>
-            NOT FOUND
+        <div className={`h-screen w-screen flex flex-col items-center justify-center`}>
+            <h4>404: NOT FOUND</h4>
+            <div  className={`h-52 w-72 relative rounded-md overflow-hidden`}>
+                <Image fill src={`https://media.tenor.com/1sWoFUxwUyEAAAAd/dog-wow.gif`} alt={`awkward dog funny gif`} />
+            </div>
+            <p>Not sure what you're looking for...</p>
+            <p>but it's not here.</p>
+            <p className="text-center">If you're not automatically sent home in {timeLeft} seconds, <Link className={`hover:underline text-blue-400`} href={`/`}>click here.</Link></p>
         </div>
+
     )
 }
