cd522bf0e85aa9cd2ca3e180e440b4cbacc7b09c
rcabre95 <raphael.cabrera111@gmail.com>
2023-05-17 23:36:06 -0500

    added Breed component for Filters, added DogCard for displaying dog information

---
+++ components/shared-ui/Breed.tsx
@@ -0,0 +1,8 @@
+export default function Breed({ breed }: { breed: string }) {
+
+    return (
+        <div>
+            {breed}
+        </div>
+    )
+}
---
+++ components/shared-ui/YouMustBeLoggedIn.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+
+export default function YouMustBeLoggedIn() {
+
+    const router = useRouter();
+    const [timeLeft, setTimeLeft] = useState<number>(10);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (timeLeft > 0) {
+                setTimeLeft(timeLeft - 1);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [timeLeft])
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            router.push('/')
+        }
+    }, [timeLeft])
+
+    return (
+        <div className={`h-screen w-screen flex flex-col items-center justify-center`}>
+            <h4>You must be logged in to view this page</h4>
+            <div  className={`h-52 w-72 relative rounded-md overflow-hidden`}>
+                <Image fill src={`https://media.tenor.com/wy2zHeWyf2gAAAAd/side-eye-dog-suspicious-look.gif`} alt={`suspicious dog funny gif`} />
+            </div>
+            <p>Please log in and you'll be automatically redirected back here.</p>
+            <p className="text-center">If you're not automatically sent home in {timeLeft} seconds, <Link className={`hover:underline text-blue-400`} href={`/`}>click here.</Link></p>
+        </div>
+
+    )
+}
