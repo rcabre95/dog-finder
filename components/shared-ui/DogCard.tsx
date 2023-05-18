856a36db29d6f329353155de315a64c643a14b5f
rcabre95 <raphael.cabrera111@gmail.com>
2023-05-17 23:59:14 -0500

    added heart to DogCard

--- components/shared-ui/DogCard.tsx
+++ components/shared-ui/DogCard.tsx
@@ -7,8 +7,8 @@ export default function DogCard(props: Dog) {
     return (
         <div className={`w-52 h-72 flex flex-col my-4 border rounded-md overflow-hidden shadow-sm`}>
             <div className={`relative w-full h-2/3`}>
-                <button className={`absolute top-2 left-2`}>
-                    <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z"/></svg>
+                <button className={`absolute top-2 right-2 z-10 h-6 w-8`}>
+                    <svg fill="#D3D3D3" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fill-rule="nonzero"/></svg>
                 </button>
                 <Image fill src={img} alt={`image of ${name}`} />
             </div>
