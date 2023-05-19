f38a9028ae5ba849e3c50bddf1a67df164d0fc98
rcabre95 <raphael.cabrera111@gmail.com>
2023-05-18 20:19:52 -0500

    added necessary states to dogs page

--- pages/dogs.tsx
+++ pages/dogs.tsx
@@ -11,35 +11,23 @@ import { Geo, MapPoint } from "@/lib/utils/distance";
 import LogoutBtn from "@/components/shared-ui/LogoutBtn";
 import YouMustBeLoggedIn from "@/components/shared-ui/YouMustBeLoggedIn";
 import Loader from "@/components/shared-ui/Loader";
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-
-//     return {
-//         props: {
-//             initBreeds: breeds,
-//             dogs: dogs
-//         }
-//     }
-// }
+import ConfirmLogOut from "@/components/ConfirmLogOut";
 
 export type AgeRange = [number, number];
 export type TSortDir = "asc" | "desc"
-
 export interface IBreed {
     name: string,
     selected: boolean
 }
 
-
-// export default function Dogs({ dogs, initBreeds }: { dogs: Array<Dog>, initBreeds: Array<string> }) {
 export default function Dogs() {
-    const [loadingDogs, setLoadingDogs] = useState<boolean>(true)
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
-    const [total, setTotal] = useState<number | null>()
+    const [total, setTotal] = useState<number | null>();
+    const [showConf, setShowConf] = useState<boolean>(false);
     const [breeds, setBreeds] = useState<Array<IBreed>>([]);
     const [distance, setDistance] = useState<number>(20);
     const [sortDirection, setSortDirection] = useState<TSortDir>("asc");
-    const [favorites, setFavorites] = useState<Array<Dog>>([])
+    const [favorites, setFavorites] = useState<Array<string>>([])
     const [page, setPage] = useState<number>(1);
     const [locationAvailable, setLocationAvailable] = useState<boolean>(true);
     const [location, setLocation] = useState<MapPoint>({ lat: 41.881832, lon: -87.623177 })
@@ -55,8 +43,30 @@ export default function Dogs() {
 
     }
 
+    const handleFavorites = (id: string) => {
+        let favoritesCopy = JSON.parse(JSON.stringify(favorites));
+        if (favoritesCopy.includes(id)) {
+            console.log("already favorited... removing")
+            const idx: number = favoritesCopy.indexOf(id);
+            favoritesCopy.splice(idx, 1);
+            console.log(favoritesCopy)
+            setFavorites(favoritesCopy);
+            console.log(favorites)
+        } else {
+            if (favorites.length > 9) {
+                alert("You have run out of favorites")
+            }
+            console.log("not yet favorited... adding")
+            favoritesCopy.push(id);
+            console.log(favoritesCopy);
+            setFavorites(favoritesCopy);
+            console.log(favorites)
+        }
+        console.log(favorites)
+    }
+
     const confirmFilters = async (newBreeds: Array<IBreed>, newAgeRange: AgeRange, newDistance: number) => {
-        setLoadingDogs(true);
+        setDogs([]);
         setBreeds(newBreeds);
         setAgeRange(newAgeRange);
         setDistance(newDistance);
@@ -70,7 +80,6 @@ export default function Dogs() {
         const newDogs = await SDK.getDogs(deconstructedNewBreeds, newZipCodes, sortDirection, newAgeRange[0], newAgeRange[1]);
 
         setDogs(newDogs);
-        setLoadingDogs(false);
         setShowFilters(false);
     }
 
@@ -114,14 +123,19 @@ export default function Dogs() {
                 
             }
         });
-
-        setLoadingDogs(false)
     }, [])
 
     return (
+
         isLoggedIn ? 
         <div className={`w-screen h-fit`}>
-            <Header showFilters={showFilters} setShowFilters={setShowFilters}></Header>
+            <Header
+                needsConf={true}
+                showFilters={showFilters}
+                setShowFilters={setShowFilters}
+                setShowConf={setShowConf}
+            />
+            <ConfirmLogOut showConf={showConf} setShowConf={setShowConf} />
             <div>
                 {total ? <h5>Search yielded {total} results</h5>: null}
                 <p>You have {10 - favorites.length} hearts left before being matched</p>
@@ -136,19 +150,22 @@ export default function Dogs() {
                         age={dog.age}
                         breed={dog.breed}
                         zip_code={dog.zip_code}
+                        favorites={favorites}
+                        handleFavorites={handleFavorites}
                     />
                 ))
                 : <Loader />}
             </div>
             {/* <pre>{JSON.stringify(dogs, null, 4)}</pre> */}
-            {showFilters ?
                 <Filters
                     breeds={breeds}
                     ageRange={ageRange}
                     distance={distance}
+                    showFilters={showFilters}
+                    setShowFilters={setShowFilters}
                     confirmFilters={confirmFilters}
-                /> : null
-            }
+                />
+            
         </div>
         : <YouMustBeLoggedIn />
     )
