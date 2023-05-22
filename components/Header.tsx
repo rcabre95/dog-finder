579d1eb6bd7e07f9503da791084037ff7a76eb34
rcabre95 <raphael.cabrera111@gmail.com>
2023-05-21 21:17:32 -0500

    changed background color to white

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header({ breeds, needsConf, showFilters, setShowFilters,
 
     
     return (
-        <header className={`w-full h-16 bg-purple-300 flex items-center justify-between fixed z-40`}>
+        <header className={`w-full h-16 bg-white flex items-center justify-between fixed z-40`}>
             <h3 className={`pl-2 text-xl`}>Furry Friend Finder</h3>
             <div className={` w-36 flex justify-between mr-2`}>
                 <button className={`border rounded-sm shadow-sm bg-white disabled:bg-slate-400 w-16 h-8`} disabled={disabled()} onClick={toggleFilters} >Filters</button>
