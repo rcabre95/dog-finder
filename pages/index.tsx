8c6d89a7e9b9f863186ae4c2133f32c74c2b686d
rcabre95 <raphael.cabrera111@gmail.com>
2023-05-24 12:17:39 -0500

    added mobile warning to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,24 @@ import Footer from '@/components/shared-ui/Footer'
 import { Steps } from '@/components/Steps'
 import IntroBanner from '@/components/IntroBanner'
 import { useRef, ForwardedRef } from 'react'
+import { useState, useEffect } from "react"
 import MetaTags from '@/components/shared-ui/MetaTags'
+import MobileWarning from '@/components/shared-ui/MobileWarning'
 
 export default function Home() {
 
   const stepsRef: ForwardedRef<HTMLElement> = useRef<HTMLElement>(null);
+  const [showWarning, setShowWarning] = useState<boolean>(false);
 
-  
+  useEffect(() =>{
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 769) {
+      setShowWarning(true)
+    }
+  }, [])
 
   return (
-    <div className={`flex flex-col h-fit`}>
+    <div className={`flex flex-col h-fit relative`}>
       <MetaTags
         title="Furry Friend Finder"
         pageUrl={process.env.NEXT_PUBLIC_HOME_URL!}
@@ -20,6 +28,7 @@ export default function Home() {
         imgUrl="/images/logo.png"
         keywords="dog, dogs, pet, pet shop, near me, pet shop near me, buy dog, buy dog near me, animals"
       />
+      <MobileWarning showWarning={showWarning} setShowWarning={setShowWarning} />
       <main className={`h-fit w-screen flex flex-col items-center justify-center bg-cream`}>
         <IntroBanner stepsRef={stepsRef} />
         <Steps ref={stepsRef} />
